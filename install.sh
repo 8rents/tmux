d824c8611136699e969e1cb479352608881395cf
Brenton Holiday <8rents@gmail.com>
2026-01-02 10:45:12 -0800

    started installer.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/data/data/com.termux/files/usr/bin
+
+# ====================
+# Install tmux configs
+# ====================
+
+# Clone this repo to ~/.config/tmux
+git clone https://github.com/8rents/tmux ~/.config/tmux
+
+# Clone TPM into the plugins folder
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+
